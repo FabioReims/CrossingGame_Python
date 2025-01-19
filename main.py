d9dfb62424bdb5dd346c162bb86843d7d3847a40
Fabio Reims <41310433+FabioReims@users.noreply.github.com>
2025-01-19 13:16:38 +0900

    Final Commit
    Finalized Game

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ screen.title("Crossing Game")
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkeypress(player.go_up, "Up")
@@ -18,3 +20,17 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
--- player.py
+++ player.py
@@ -11,9 +11,18 @@ class Player(Turtle):
         self.shape("turtle")
         self.color("black")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
     def go_up(self):
         self.setheading(90)
         self.forward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
